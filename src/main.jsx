c70eece5adaa635e7636945e45c13e62f87dd490
Johan Yindou <jyindou@gmail.com>
2023-11-20 10:47:32 +0100

    Création des routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Accueil from './pages/App.jsx';
+import Compétances from './pages/Compétances.jsx'
+import Contact from './pages/Contact.jsx'
+import Portfolio from './pages/Portfolio.jsx'
 import '../styles/index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Accueil />,
+  },
+  {
+    path: '/compétances',
+    element: <Compétances />,
+  },
+  {
+    path: '/portfolio',
+    element: <Portfolio />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={ router } />
+  </React.StrictMode>
+);
