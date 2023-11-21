26aff07ca51a094ecf621dcb7b425e4ad389c0d7
Johan Yindou <jyindou@gmail.com>
2023-11-21 16:46:19 +0100

    Création du composant Navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-    
-
-    return (
-        <header>
-            <div className="navbar">
-                <ul>
-                    <li className="navbar-item">Accueil</li>
-                    <li className="navbar-item">Compétances</li>
-                    <li className="navbar-item">Portfolio</li>
-                    <li className="navbar-item">Contact</li>
-                </ul>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="navbar">
+        <ul className="navbar-list">
+          <Link to="/">
+            <li className="navbar-item">Accueil</li>
+          </Link>
+          <Link to="/compétances">
+            <li className="navbar-item">Compétances</li>
+          </Link>
+          <Link to="/portfolio">
+            <li className="navbar-item">Portfolio</li>
+          </Link>
+          <Link to="/contact">
+            <li className="navbar-item">Contact</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
