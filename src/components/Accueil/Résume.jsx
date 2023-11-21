5391ac6dd83930e16414deb9bbd2da704e0a993a
Johan Yindou <jyindou@gmail.com>
2023-11-21 16:51:20 +0100

    Création du composant de la page Accueil

---
+++ src/components/Accueil/Résume.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Resume = () => {
+    return(
+        <section className="resume">
+            <img className="profile" src="../../../public/media/profile.png" alt="profil" />
+            <div className="resume-item">
+                <h1 className="nom">Je m'appelle Johan Yindou</h1>
+                <p className="metier">Concepteur / Développeur d'application</p>
+            </div>
+        </section>
+    )
+};
+export default Resume;
